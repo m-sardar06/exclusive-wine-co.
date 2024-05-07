fe8119d1aef313a2f4a3bd91d1a3f3a1299b2aa8
Mehr Bano Sardar <121179378+m-sardar06@users.noreply.github.com>
2024-05-08 00:39:50 +0500

    Add files via upload

---
+++ wine-store-admin/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <hr />
+      <p>Exclusive Wine & Co. | Copyright @ 2024 - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
